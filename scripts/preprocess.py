38c1143fdbb2261c49ce307160da62c316c9755e
Agam Sanghera <agamsanghera911@gmail.com>
2024-12-07 12:36:11 -0800

    Create analysis.py script with comand line arguments

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -16,6 +16,8 @@ from sklearn.compose import ColumnTransformer, make_column_selector
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression 
 from sklearn.model_selection import cross_validate, RandomizedSearchCV
+import warnings
+warnings.filterwarnings(action='ignore')
 
 @click.command()
 @click.option('--raw_data', type=str, help="Raw data location")
@@ -27,25 +29,25 @@ def main(raw_data, preprocessed_data, preprocessor_loc, seed):
     os.makedirs(os.path.dirname(preprocessed_data), exist_ok=True)
     os.makedirs(os.path.dirname(preprocessor_loc), exist_ok=True)
 
-    # the Dates column is parsed through date argument to make sure it is seen as datetime object.
+    # the Dates column is parsed through date argument to make sure it is seen as datetime object
     ttc = pd.read_csv(raw_data, parse_dates=['Date'])
 
     print(f"This dataset has {ttc.shape[0]} rows and {ttc.shape[1]} columns")
 
-    # Let's also fix the 'Time' column
+    # Fix the 'Time' column
     ttc1 = ttc.copy() # preserving the original
 
-    # Converting column to datetime object
+    # Convert column to datetime object
     ttc1['Time'] = pd.to_datetime(ttc['Time']).dt.time
 
     ttc1['Date_'] = ttc1['Date'].dt.date
     ttc1['Month'] = ttc1['Date'].dt.month
     ttc1['Hour'] = ttc1['Time'].map(lambda x: x.hour) # used ChatGPT for this conversion
 
-    # Let's drop the 'Date' and 'Time' column since they are no longer needed
+    # Drop the 'Date' and 'Time' column since they are no longer needed
     ttc1 = ttc1.drop(columns=['Date', 'Time'])
 
-    # Drop 'Direction' and 'Vehicle' columns as  they are not useful
+    # Drop 'Direction' and 'Vehicle' columns as they are not useful
     ttc_clean = ttc1.drop(columns=['Direction', 'Vehicle'])
     # As NaN routes have 0 delays, it is safe to drop these rows
     ttc_clean = ttc_clean.dropna()
@@ -54,22 +56,22 @@ def main(raw_data, preprocessed_data, preprocessor_loc, seed):
     #Remove outliers with delay greater than 30 mins and less than 1 min
     ttc_lr = ttc_clean.loc[(ttc_clean["Min Delay"]<30) & (ttc_clean["Min Delay"]>0)].reset_index(drop=True)
     
-    #store data without outliers
+    # Store data without outliers
     ttc_lr.to_csv(os.path.join(preprocessed_data,'no_outliers.csv'),index=False)
     # Assign delays into 3 classes "Short Delay", "Medium Delay", and "Long Delay"
     ttc_lr["Min Delay"] = ttc_lr["Min Delay"].apply(lambda x: "Short Delay" if type(x)== int and x >0 and x<=10 else x)
     ttc_lr["Min Delay"] = ttc_lr["Min Delay"].apply(lambda x: "Medium Delay" if type(x)== int and  x >10 and x<=20 else x)
     ttc_lr["Min Delay"] = ttc_lr["Min Delay"].apply(lambda x: "Long Delay" if type(x)== int and  x >=10 else x)
 
-    #Split targets from features
+    # Split targets from features
     X = ttc_lr[["Route","Incident","Location","Day","Hour","Month"]]
     y = ttc_lr['Min Delay']
 
-    #Define feature types
+    # Define feature types
     numeric_features=["Hour","Month"]
     categorical_features = ['Location', 'Route', 'Incident',"Day"]
 
-    #Create transformer and preprocessing pipeline
+    # Create transformer and preprocessing pipeline
     preprocessor = ColumnTransformer(
     transformers=[
         ('num', StandardScaler(), numeric_features),
@@ -77,12 +79,12 @@ def main(raw_data, preprocessed_data, preprocessor_loc, seed):
     ]
     )
 
-    #Split into test train sets and save to disk
+    # Split into test train sets and save to disk
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
-    X_train.to_csv(os.path.join(preprocessed_data,'X_train.csv'))
-    y_train.to_csv(os.path.join(preprocessed_data,'y_train.csv'))
-    X_test.to_csv(os.path.join(preprocessed_data,'X_test.csv'))
-    y_test.to_csv(os.path.join(preprocessed_data,'y_test.csv'))
+    X_train.to_csv(os.path.join(preprocessed_data,'X_train.csv'),index=False)
+    y_train.to_csv(os.path.join(preprocessed_data,'y_train.csv'),index=False)
+    X_test.to_csv(os.path.join(preprocessed_data,'X_test.csv'),index=False)
+    y_test.to_csv(os.path.join(preprocessed_data,'y_test.csv'),index=False)
     pickle.dump(preprocessor,open(os.path.join(preprocessor_loc,'delay_preprocessor.pickle'),'wb'))
     preprocessor.fit(X_train)
     train_processed=preprocessor.transform(X_train)
